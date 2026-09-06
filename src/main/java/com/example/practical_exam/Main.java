cf19771866c7a955b017c8b1d3ad8ece99fe86f6
an6953@g.rit.edu <an6953@rit.edu>
2026-09-06 12:12:34 +0500

    Создать функцию ReverseLetter

--- src/main/java/com/example/practical_exam/Main.java
+++ src/main/java/com/example/practical_exam/Main.java
@@ -1,7 +1,8 @@
 package com.example.practical_exam;
 
-public class Main {
+public class Main extends ReverseLetter{
     public static void main(String[] args) {
         String word = "Hello , World!";
+        System.out.println(ReverseLetter.reverseLetter(word));
     }
 }
---
+++ src/main/java/com/example/practical_exam/ReverseLetter.java
@@ -0,0 +1,25 @@
+package com.example.practical_exam;
+
+public class ReverseLetter {
+    public static String reverseLetter(String word){
+        if (word.isBlank()){
+            return " ";
+        }
+        char[] chars = word.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            if ( !Character.isLetter(chars[left])) {
+                left++;
+            } else if ( !Character.isLetter(chars[right])) {
+                right--;
+            }
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
+}
