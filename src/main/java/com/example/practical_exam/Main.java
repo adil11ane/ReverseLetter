ba6896df5b583a13875223f83bf63b927a652c81
an6953@g.rit.edu <an6953@rit.edu>
2026-09-11 17:26:47 +0500

    Сделать тесты на функцию reverseLetter()

--- src/main/java/com/example/practical_exam/Main.java
+++ src/main/java/com/example/practical_exam/Main.java
@@ -2,7 +2,7 @@ package com.example.practical_exam;
 
 public class Main {
     public static void main(String[] args) {
-        String word = "Hello , World!";
+        String word = "J@va the be$t!123";
         System.out.println(ReverseLetter.reverseLetter(word));
     }
 }
--- src/test/java/com/example/practical_exam/PracticalExamApplicationTests.java
+++ src/test/java/com/example/practical_exam/PracticalExamApplicationTests.java
@@ -1,13 +1,59 @@
 package com.example.practical_exam;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+
 class PracticalExamApplicationTests {
 
+
+
     @Test
-    void contextLoads() {
+    void regularReverseLetterCase() {
+        String res = ReverseLetter.reverseLetter("J@va the be$t!123");
+        assertEquals("t@eb eht av$J!123", res);
     }
 
+
+    @Test
+    void blankStringCase() {
+        String res = ReverseLetter.reverseLetter(" ");
+        assertEquals(" ", res);
+    }
+
+    @Test
+    void oneLetterCase() {
+        String res = ReverseLetter.reverseLetter("a");
+        assertEquals("a", res);
+    }
+
+    @Test
+    void stringWithoutLettersCase() {
+        String res = ReverseLetter.reverseLetter("123 !@#");
+        assertEquals("123 !@#", res);
+    }
+
+    @Test
+    void onlyLettersCase() {
+        String res = ReverseLetter.reverseLetter("abcd");
+        assertEquals("dcba", res);
+    }
+
+    @Test
+    void nonAlphabeticCharactersAtTheEdgesAndMiddleCase() {
+        String res = ReverseLetter.reverseLetter("!@#$%^&*()_+");
+        assertEquals("!@#$%^&*()_+", res);
+    }
+
+    @Test
+    void nullInputCase() {
+        String res = ReverseLetter.reverseLetter(null);
+        assertEquals(" " , res);
+    }
+
+    @Test
+    void lettersChangesWithTheirRegisteryCase() {
+        String res = ReverseLetter.reverseLetter("AsdFgHasl");
+        assertEquals("lsaHgFdsA", res);
+    }
 }
